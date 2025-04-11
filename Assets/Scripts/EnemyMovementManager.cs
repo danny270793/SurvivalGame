ec293fc4170aa3badecb62ba90fc3a9202498910
Danny Vaca <danny270793@icloud.com>
2025-04-11 17:27:06 -0500

    change color bassed on life

--- Assets/Scripts/EnemyMovementManager.cs
+++ Assets/Scripts/EnemyMovementManager.cs
@@ -2,6 +2,26 @@ using UnityEngine;
 
 public class EnemyMovementController : MonoBehaviour
 {
+    // life
+    public int life = 100;
+
+    void changeColorBasedOnLife()
+    {
+        if(life > 50 && life < 80)
+        {
+            GetComponent<Renderer>().material.color = Color.yellow;
+        }
+        else if(life <= 50)
+        {
+            GetComponent<Renderer>().material.color = Color.red;
+        }
+        else if(life <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    // movement
     public float moveSpeed = 10f;
 
     void moveForward()
@@ -11,13 +31,15 @@ public class EnemyMovementController : MonoBehaviour
     }
 
     // main
+
     void Start()
     {
-        
+
     }
 
     void Update()
     {
         moveForward();
+        changeColorBasedOnLife();
     }
 }
